592b85b16212628bc09073befc883d9f2302233e
Yu Sun <sunyu1357@gmail.com>
2016-09-22 12:15:14 +0000

    sscu_2.0.0
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/sscu@121236 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/genomic_gc3.R
+++
@@ -1,8 +0,0 @@
-genomic_gc3 <- function(inputfile){
-  
-  genomic_cds <- Biostrings::readDNAStringSet(inputfile)
-  genomic_cat <- seqinr::s2c(as.character(BiocGenerics::unlist(genomic_cds)))
-  genomic_gc3 <- seqinr::GC3(genomic_cat)    
-  
-  return(genomic_gc3)
-}
